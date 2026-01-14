c793933239e3fc961a44f3db97bbb0cc187d56a7
sivashankar0398-cmyk <sivashankar0398@gmail.com>
2026-01-14 06:07:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+import os
 
 
 # Load the exported scaler and scaled model
@@ -74,4 +75,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
